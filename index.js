c937080919e30c4285a2a3c69333e71b8482b527
Teja Kummarikuntla <34749692+tejakummarikuntla@users.noreply.github.com>
2024-01-31 11:37:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello!" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Willing!!!" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
